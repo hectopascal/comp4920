95fbbc84f4ad663230c59b9dfe4e22d0643a6ec4
Timothy Gibson <tim@timgibson.com>
2018-09-07 18:14:25 +1000

    work on style, star rating

--- www/index.php
+++ www/index.php
@@ -2,6 +2,9 @@
 	<head>
 		<!-- bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!-- Font Fonts -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 		<!-- Bootrap JS & JQuery -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -10,14 +13,28 @@
 	</head>
 
 	<body class="bg-light" style="padding-top:5rem;">
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-			<a class="navbar-brand" href="#">The Comp Elective Machine</a>
-			<div class="collapse navbar-collapse">
-				<ul class="navbar-nav mr-auto"></ul>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#ef6c30">
+			<a class="navbar-brand " style="font-family:'Poppins'; font-size:24px; font-weight:900; font-style:italic" href="#">Rear Window</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+			 aria-controls="navbar" aria-expanded="false" aria-label="Toggle Navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbar">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">About</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+
 				<form class="form-inline my-2 my-lg-0">
 					<input class="form-control mr-sm-2" type="text" placeholder="Course name / code">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					<button class="btn btn-light my-2 my-sm-0 mr-2">Search</button>
+					<button class="btn btn-light my-2 my-sm-0">Login</button>
 				</form>
+
 			</div>
 		</nav>
 
--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 
-function appendCourse(c_title, c_info)
+function appendCourse(c_title, c_info, rating)
 {
 	var celem  = document.createElement('div');
 	var cnode  = document.createElement('h4');
@@ -11,6 +11,25 @@ function appendCourse(c_title, c_info)
 	cnode1.appendChild(info);
 	celem.appendChild(cnode1);
 
+	var cratings = document.createElement('div');
+	var crating = document.createElement('span');
+	for(i = 0; i < 5; i++)
+	{
+
+		var irating = document.createElement('i');
+		crating.appendChild(irating);
+		if(rating >= 1)
+			irating.setAttribute('class', 'fas fa-star');
+		else if(rating == 0.5)
+			irating.setAttribute('class', 'fas fa-star-half-alt');
+		else
+			irating.setAttribute('class', 'far fa-star');
+			
+		rating--;
+		irating.setAttribute('style', 'color:#ef6c30');
+	}
+	celem.appendChild(crating);
+
 	celem.setAttribute("class", 'p-3 bg-white');
 	celem.setAttribute("style", 'margin-top:20px;margin-bottom:20px; box-shadow:0 0 4px 0 rgba(0,0,0,0.2); border-radius:10px;');
 
@@ -19,14 +38,14 @@ function appendCourse(c_title, c_info)
 
 function main()
 {
-	appendCourse('COMP1511: Programming Fundamentals', 'An introduction to problem-solving via programming, which aims to have students develop proficiency in using a high level programming language.');
-	appendCourse('COMP2521: Data Structures and Algorithms', 'The goal of this course is to deepen students\' understanding of data structures and algorithms and how these can be employed effectively in the design of software systems.');
-	appendCourse('COMP1521: Computer Systems Fundamentals', 'This course provides a programmer\'s view on how a computer system executes programs, manipulates data and communicates.');
-	appendCourse('COMP1531: Software Engineering Fundamentals', 'This course provides an introduction to software engineering principles: basic software lifecycle concepts, modern development methodologies, conceptual modeling and how these activities relate to programming.');
-	appendCourse('COMP3121: Algorithms and Programming Techniques', 'Correctness and efficiency of algorithms. Computational complexity: time and space bounds. Techniques for best-case, worst-case and average-case time and space analysis.')
-	appendCourse('COMP2511: Object-Oriented Design & Programming', 'This course aims to introduce students to the principles of object-oriented design and to fundamental techniques in object-oriented programming.');
-	appendCourse('COMP4920: Management and Ethics', 'This course will develop a framework on which management and ethical issues can be developed.');
-	appendCourse('COMP3900: Computer Science Project', 'A capstone software project. Students work in teams to define, implement and evaluate a real-world software system.');
+	appendCourse('COMP1511: Programming Fundamentals', 'An introduction to problem-solving via programming, which aims to have students develop proficiency in using a high level programming language.', 1);
+	appendCourse('COMP2521: Data Structures and Algorithms', 'The goal of this course is to deepen students\' understanding of data structures and algorithms and how these can be employed effectively in the design of software systems.', 1.5);
+	appendCourse('COMP1521: Computer Systems Fundamentals', 'This course provides a programmer\'s view on how a computer system executes programs, manipulates data and communicates.', 4.5);
+	appendCourse('COMP1531: Software Engineering Fundamentals', 'This course provides an introduction to software engineering principles: basic software lifecycle concepts, modern development methodologies, conceptual modeling and how these activities relate to programming.', 4);
+	appendCourse('COMP3121: Algorithms and Programming Techniques', 'Correctness and efficiency of algorithms. Computational complexity: time and space bounds. Techniques for best-case, worst-case and average-case time and space analysis.', 3.5)
+	appendCourse('COMP2511: Object-Oriented Design & Programming', 'This course aims to introduce students to the principles of object-oriented design and to fundamental techniques in object-oriented programming.', 3);
+	appendCourse('COMP4920: Management and Ethics', 'This course will develop a framework on which management and ethical issues can be developed.', 0);
+	appendCourse('COMP3900: Computer Science Project', 'A capstone software project. Students work in teams to define, implement and evaluate a real-world software system.', 2);
 }
 
 document.addEventListener("DOMContentLoaded", main);
