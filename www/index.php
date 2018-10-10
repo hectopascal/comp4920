7682dd4c39b6865bb0e0170e07942e7a27d0c159
gill <gillemorris@gmail.com>
2018-10-10 17:10:37 +1100

    add badwords

--- www/index.php
+++ www/index.php
@@ -12,6 +12,7 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="badwords.js"></script>
         <script src="reviews.js"></script>
         <script src="main.js"></script>
         <script src="ajaxpy.js"></script>
