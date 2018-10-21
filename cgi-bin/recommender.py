b451a9bae5f697b7820ab4e6eda9ceef3cd64752
Linda Truong <lindatruong10@gmail.com>
2018-10-21 20:20:14 +1100

    pull from db recommender

--- cgi-bin/recommender.py
+++ cgi-bin/recommender.py
@@ -11,10 +11,29 @@ from scipy.spatial.distance import cosine
 
 def main():
     # import the postgre db as csv and parse into pandas
-    # https://stackoverflow.com/questions/2987433/how-to-import-csv-file-data-into-a-postgresql-table
-    reviews = pd.read_csv('../db/course_review.csv', delimiter=',', header=0, names=['course','student','rating'])
+    try:
+       # connect to the PostgreSQL database
+       conn = psycopg2.connect(host = "cs4920.ckc9ybbol3wz.ap-southeast-2.rds.amazonaws.com",
+                               database = "cs4920",
+                               user = "gill",
+                               password = "gill")
+
+       # create a new cursor and Delete the post with id == post_id
+       cur = conn.cursor()
+       cur.execute('COPY (SELECT course, uid, rating from reviews) TO "../db/courses_review.csv" WITH CSV HEADER')
+       # records = cur.fetchall()
+
+       cur.close()
+       conn.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error,file=sys.stderr)
+        if conn is not None:
+            conn.close()
+        return "Error Error Error !!!"
+
+    reviews = pd.read_csv('../db/course_review.csv', delimiter=',', header=0, names=['course','uid','rating'])
     # print(reviews.head(5))
-    user_reviews_matrix = pd.pivot_table(reviews, index='student', columns='course', values='rating',aggfunc='first')
+    user_reviews_matrix = pd.pivot_table(reviews, index='uid', columns='course', values='rating',aggfunc='first')
 
     # user_reviews_matrix = user_reviews_matrix.fillna(0) // Fill empty spaces with 0
 
@@ -32,6 +51,9 @@ def main():
     for i in range(0, len(title_similarities)):
         recommendations.iloc[i,:20] = title_similarities.iloc[0:,i].sort_values(ascending=False)[:20].index
     recommendations.to_csv('../db/course_recommendations.csv')
+
+    # https://stackoverflow.com/questions/2987433/how-to-import-csv-file-data-into-a-postgresql-table
+
 
 if (__name__ == '__main__'):
     main();
